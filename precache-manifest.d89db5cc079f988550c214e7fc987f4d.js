39a465afcd77f7f045f3de7306d0ee76690a3d6b
shakil <shakhout@alfadocs.com>
2020-06-04 21:05:36 +0200

    Updates

---
+++ precache-manifest.d89db5cc079f988550c214e7fc987f4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332cbaa97a33d6f991701135ff2fb99a",
+    "url": "/tlcl/index.html"
+  },
+  {
+    "revision": "52ceb83d8a6a36cafcd7",
+    "url": "/tlcl/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1416f41c1ef0b0344fb3",
+    "url": "/tlcl/static/js/2.1b3aa4c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tlcl/static/js/2.1b3aa4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ceb83d8a6a36cafcd7",
+    "url": "/tlcl/static/js/main.0dd2e0b3.chunk.js"
+  },
+  {
+    "revision": "785ed229fe48607e568c",
+    "url": "/tlcl/static/js/runtime-main.e232d751.js"
+  }
+]);
