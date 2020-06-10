4435fd44ac7e2c5741528e5efeb0d07c67a332b5
shakil <shakhout@alfadocs.com>
2020-06-11 01:50:43 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tlcl/precache-manifest.d89db5cc079f988550c214e7fc987f4d.js"
+  "/tlcl/precache-manifest.f164b74d39a98b58e6295019f3d0054d.js"
 );
 
 self.addEventListener('message', (event) => {
